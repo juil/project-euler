77666b58999b704ab92a77bee61ee58930b1649c
Juil Yoon <jiy.juil@gmail.com>
2016-04-14 00:23:08 +0900

    Faster, but not fast enough

--- p003.py
+++ p003.py
@@ -6,7 +6,7 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 #
 # What is the largest prime factor of the number 600851475143 ?
-
+import math
 def isPrime(n):
     '''Check if n is a prime number
     Take number n as input
@@ -26,39 +26,58 @@ def primeFactors(n):
     '''List all prime factors of n
     Takes number n as input
     Returns a list of numbers'''
-    # TODO:0 Make primeFactors more efficient
     r = []
-    # Ignore for speed
-    # if n%2 == 0:
-    #     r.append(2)
-    for i in range(1,n/2+1, 2):
+    if n%2 == 0:
+        r.append(2)
+    for i in range(1, n/2+1, 2):
         if n%i == 0 and isPrime(i):
             r.append(i)
     return r
 
-# FIXME: Infinite loop?
+# DONE:0 Fix looping problem
 def largestPrime(n):
     '''Faster version for only the largest prime factor
-    Returns number'''
-    l = 1
-    for i in range(1,n/2+1, 2):
-        print l
-        if n%i == 0 and isPrime(i):
-            l = i
-            print l
-    return l
+    Returns number
+
+    Start by going backwards from largest factor '''
+    largest = 0
+    for i in range(1, int(math.sqrt(n)/2)):
+        if n%i == 0:
+            if isPrime(n/i):
+                return n/i
+            # elif isPrime(i): #comment out for speed
+            #     largest = i
+    return largest
+
+    # Slow method
+    # l = 1
+    # for i in range(1, n/2+1, 2):
+    #     if n%i == 0 and isPrime(i):
+    #         l = i
+    # return l
 
 # Test
 print primeFactors(5)
 #>>> []
 print primeFactors(10)
 #>>> [2,5]
-print primeFactors(13195)
+t=13195
+print "Find:", t
+print primeFactors(t)
+print largestPrime(t)
 #>>> [5, 7, 13, 29]
-print primeFactors(12087123)
-# print largestPrime(12087123)
+t=12087123
+print "Find:", t
+print primeFactors(t)
+print largestPrime(t)
 #>>> [3, 37, 108893]
+t=1208712371
+print "Find:", t
+# print primeFactors(t)
+print largestPrime(t)
+#>>>
 
 # Solution
-# print primeFactors(600851475143) # Takes too long
-# print largestPrime(600851475143)
+s = 600851475143
+# print primeFactors(s) # Takes too long
+# print largestPrime(s)
